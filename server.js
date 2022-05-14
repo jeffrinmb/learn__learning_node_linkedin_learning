6096fcfb79aa2075387d302c41f1a8e86e58b157
Jeffrin Mathew Benny <jeffrinmathewbenny@gmail.com>
2022-05-14 17:01:14 +0530

    Added Mongoose and DotEnv to project, added mongo connection

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+const dbUrl = `mongodb+srv://${process.env.MONGO_DB_USER_NAME}:${process.env.MONGO_DB_USER_PASSWORD}@cluster0.fferj.mongodb.net/learning-node?retryWrites=true&w=majority`;
 
 const messages = [
   { name: 'Tim', message: 'Hello!' },
@@ -26,6 +29,14 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => { // eslint-disable-line
   console.log('A User Connected!');
+});
+
+mongoose.connect(dbUrl, (err) => {
+  if (err) {
+    console.log('Mongo DB Connection Error', err);
+  } else {
+    console.log('Mongo DB Connection Successful');
+  }
 });
 
 const server = http.listen(3000, () => {
